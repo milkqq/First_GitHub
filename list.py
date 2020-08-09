fdf415d877182865c867c52718c10b0c5e57e84a
Mike Lin <go.milk.qq@gmail.com>
2020-08-09 08:51:26 +0800

    AddNew file

---
+++ list.py
@@ -0,0 +1,11 @@
+a = [1,2,3,4,5]
+b = [2,3,5,'abc',[99,'c','你']]
+print(a[2])
+print(b)
+
+print(a[0],   a[2], a[4], sep="~")
+
+list1 = [1, 2, 3, 2, 3, 4, 2]
+print("2一共出現{}次".format(list1.count(2)))
+
+
